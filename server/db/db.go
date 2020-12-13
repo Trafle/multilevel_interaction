f657189984d286408305bca225fca8d674cf9995
MaryLynJuana <mary_juana@ukr.net>
2020-12-13 14:37:45 +0200

    fix(core): Fix DB connection

--- server/db/db.go
+++ server/db/db.go
@@ -1,34 +1,21 @@
 package db
 
 import (
+	"fmt"
 	"database/sql"
-	"net/url"
-
-	// _ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Connection struct {
 	DbName         string
 	User, Password string
 	Host           string
-	DisableSSL     bool
 }
 
-func (c *Connection) ConnectionURL() string {
-	dbUrl := &url.URL{
-		Scheme: "payment_system",
-		Host:   c.Host,
-		User:   url.UserPassword(c.User, c.Password),
-		Path:   c.DbName,
-	}
-	if c.DisableSSL {
-		dbUrl.RawQuery = url.Values{
-			"sslmode": []string{"disable"},
-		}.Encode()
-	}
-	return dbUrl.String()
+func (c *Connection) formatConnection() string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s", c.User, c.Password, c.Host, c.DbName)
 }
 
 func (c *Connection) Open() (*sql.DB, error) {
-	return sql.Open("postgres", c.ConnectionURL())
+	return sql.Open("mysql", c.formatConnection())
 }
