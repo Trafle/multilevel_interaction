b201f6cdbec28bf617e6d78ddd9da5330463b3bf
AntonPodash <apodash01@gmail.com>
2020-12-13 14:31:59 +0200

    1st scenario

--- client/examples.js
+++ client/examples.js
@@ -1,32 +1,16 @@
-// This file contains examples of scenarios implementation using
-// the SDK for channels management.
+const { Client } = require("./accounts/client");
 
-const channels = require('./channels/client');
+const client = Client("http://localhost:8080");
 
-const client = channels.Client('http://localhost:8080');
-
-// Scenario 1: Display available channels.
-client.listChannels()
-    .then((list) => {
-        console.log('=== Scenario 1 ===');
-        console.log('Available channels:');
-        list.forEach((c) => console.log(c.name));
-    })
-    .catch((e) => {
-        console.log(`Problem listing available channels: ${e.message}`);
-    });
-
-// Scenario 2: Create new channel.
-client.createChannel('my-new-channel')
-    .then((resp) => {
-        console.log('=== Scenario 2 ===');
-        console.log('Create channel response:', resp);
-        return client.listChannels()
-            .then((list) => list.map((c) => c.name).join(', '))
-            .then((str) => {
-                console.log(`Current channels: ${str}`);
-            })
-    })
-    .catch((e) => {
-        console.log(`Problem creating a new channel: ${e.message}`);
-    });
+(async () => {
+    // Scenario 1: Display a list of accounts with a balance on them and the time of the last transaction performed on the account
+   let accounts;
+    console.log("=== Scenario 1 ===");
+    try {
+        accounts = await client.fetchAccounts();
+        console.log("Accounts with a balance on them:");
+        console.table(accounts);
+    } catch (err) {
+        console.log(`Problem listing accounts: `, err);
+    }
+})();
